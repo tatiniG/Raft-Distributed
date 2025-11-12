90f603a061cf5814de0b4049d7e9957152b62244
TARINI GUTTULA <65956745+tatiniG@users.noreply.github.com>
2025-11-11 21:04:57 -0500

    Update Entry case class to use MemberIds

--- Entry.scala
+++ Entry.scala
@@ -1,4 +1,4 @@
 package com.nebtrx.raft
 
 // Entry should contain more log related data in order to handle log replication
-case class Entry(term: Term, leaderId: MemberId)
+case class Entry(term: Term, leaderId: MemberIds)
