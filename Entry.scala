047c95bf748c98ad47fe4ea061dae61bcee497ba
TARINI GUTTULA <65956745+tatiniG@users.noreply.github.com>
2025-12-24 11:41:19 +0530

    Update Entry.scala

--- Entry.scala
+++ Entry.scala
@@ -1,4 +1,4 @@
 package com.nebtrx.raft
-
+package com.nebtrx.raft
 // Entry should contain more log related data in order to handle log replication
 case class Entry(term: Terms, leaderId: MemberId)
